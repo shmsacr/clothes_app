b9fd22eb6d5d60a8e30415ed4cb40253d366f0a5
shmsacr <shmsacr@gmail.com>
2023-09-14 23:19:36 +0300

    feat: add api connection class

---
+++ lib/api_connection/api_connection.dart
@@ -0,0 +1,6 @@
+class API {
+  static const hostConnect = "http://192.168.1.108/api_clothes_store";
+  static const hostConnectUser = "$hostConnect/user";
+  static const validateEmail = "$hostConnect/user/validate_email.php";
+  static const signUp = "$hostConnect/user/signup.php";
+}
